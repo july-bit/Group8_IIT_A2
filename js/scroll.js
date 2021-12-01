91d60ec19895abf2438c0702f1c9d6301e797b09
anhthu106 <phamthu10602@gmail.com>
2021-12-01 13:52:31 +0700

    First commit

---
+++ js/scroll.js
@@ -0,0 +1,33 @@
+ScrollReveal({
+    reset:false,
+    duration: 2000,
+})
+
+ScrollReveal().reveal('.overview', { 
+    delay: 300,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.motivation', {
+    delay: 500,
+    origin: 'bottom',
+})
+ScrollReveal().reveal('.description', { 
+    delay: 700,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.tool-tech', { 
+    delay: 900,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.skill', { 
+    delay: 900,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.outcome', { 
+    delay: 900,
+    origin: 'bottom',
+});
